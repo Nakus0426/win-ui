e53acf69195e3d620023f37d7d516f11c92b2344
Nakus0426 <820774459@qq.com>
2023-05-06 17:05:27 +0800

    feat(style): add dark css variable

--- packages/components/src/button/Button.vue
+++ packages/components/src/button/Button.vue
@@ -11,9 +11,11 @@ const props = defineProps({
 </script>
 
 <template>
-  <button type="button">
-    按钮
-  </button>
+  <div style="background: url(https://img2.baidu.com/it/u=2319385315,296614944&fm=253&fmt=auto&app=138&f=PNG?w=889&h=500);width: 500px;height: 500px;">
+    <div class="button">
+      按钮
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
